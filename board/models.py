bbf5caa176973d13448c508c01cbb516d094deb1
yongwoo97 <woo22661@naver.com>
2022-03-15 21:38:51 +0900

    title null 값 허용

--- board/models.py
+++ board/models.py
@@ -5,6 +5,6 @@ class todolist(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(null=False, blank=False)
-    title = models.CharField(max_length=1000, null=True)
+    title = models.CharField(max_length=1000, null=True, blank=True)
     repeat = models.IntegerField(default=0)
     importance = models.IntegerField(default=0)
